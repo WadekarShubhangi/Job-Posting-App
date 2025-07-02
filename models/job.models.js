551204d8915c162f0634cdb7c5c6edcce8972761
WadekarShubhangi <shubhangiwadekar1096@gmail.com>
2025-07-02 19:56:48 +0530

    feat: changing qualification schema and json data.

--- models/job.models.js
+++ models/job.models.js
@@ -1,39 +1,44 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const jobSchema = new mongoose.Schema({
-      
-    jobTitle:{
-        type : String,
-        required: true,
+const jobSchema = new mongoose.Schema(
+  {
+    jobTitle: {
+      type: String,
+      required: true,
     },
     companyName: {
-        type: String,
-        required : true,
+      type: String,
+      required: true,
     },
     location: {
-        type : String,
-        required : true,
+      type: String,
+      required: true,
     },
     salary: {
-        type : Number,
-        required : true,
+      type: Number,
+      required: true,
     },
     jobType: {
-        type : String,
-        enum : ["Full-time (On-site)", "Part-time (On-site)", "Full-time (Remote)", "Part-time (Remote)"],
-        required : true,
+      type: String,
+      enum: [
+        "Full-time (On-site)",
+        "Part-time (On-site)",
+        "Full-time (Remote)",
+        "Part-time (Remote)",
+      ],
+      required: true,
     },
     jobDescription: {
-        type : String,
-        required : true,
+      type: String,
+      required: true,
     },
-    qualifications: [{type : String}]
-    
-      
-},{
-    timestamps : true
-})
+    qualifications: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Jobs = mongoose.model("Jobs", jobSchema)
+const Jobs = mongoose.model("Jobs", jobSchema);
 
-module.exports = Jobs
+module.exports = Jobs;
